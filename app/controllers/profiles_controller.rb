0a8ec6e01d8819864cd075c43722a2d4df21dbd1
mohamedsugroo <mohamedsugroo@gmail.com>
2018-06-10 14:14:34 +0100

    profile styles and bhv

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,10 +4,12 @@ class ProfilesController < ApplicationController
   end
 
   def show
+    
     @profile = Contractor.find(params[:id])
     @licences = Licence.order("created_at DESC")
     @qualifications = Qualification.order("created_at DESC")
     @qualification = Qualification.new
     @licence = Licence.new
+
   end
 end
